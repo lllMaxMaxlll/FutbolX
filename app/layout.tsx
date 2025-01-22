d1807a1e55dc99fe137daebfbfbafa876bf0246c
lllMaxMaxlll <max.herr.88@gmail.com>
2025-01-22 19:09:06 -0300

    feat: add Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
 import { ThemeProvider } from "@/components/theme-provider";
+import { Analytics } from "@vercel/analytics/react";
 
 const geistSans = Geist({
 	variable: "--font-geist-sans",
@@ -30,6 +31,7 @@ export default async function RootLayout({
 				<ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
 					{children}
 					<Toaster />
+					<Analytics />
 				</ThemeProvider>
 			</body>
 		</html>
